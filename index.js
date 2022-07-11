0b2e0d424050e975f3cf6b948c7cdb695c8f25e1
jeksilaen <88269870+jeksilaen@users.noreply.github.com>
2022-07-11 19:38:20 +0700

    added project page with video, updated color scheme

--- index.js
+++ index.js
@@ -14,3 +14,41 @@ $(".title-image").hover(function () {
     }
 );
 
+//About image hover change
+$(".about-img").hover(function () {
+    // over
+    let attr = (this.classList[1]); 
+    changeAbout(attr);
+}, function () {
+    // out
+    $(".me").attr("src", "images/me.png");
+    $(".school").attr("src", "images/school.png");
+    $(".goals").attr("src", "images/goals.png");
+}
+);
+
+//Project button hover change   
+$(".project-button").hover(function () {
+        // over
+        $(this).css("background-color", "#53524f");
+    }, function () {
+        // out
+        $(this).css("background-color", "#31302E");
+    }
+);
+
+function changeAbout(attr){
+    switch (attr) {
+        case "me":
+            $(".me").attr("src", "images/me2.png");
+            break;
+        case "school":
+            $(".school").attr("src", "images/school2.png");
+            break;
+        case "goals":
+            $(".goals").attr("src", "images/goals2.png");
+            break;
+        default:
+            break;
+    }
+}
